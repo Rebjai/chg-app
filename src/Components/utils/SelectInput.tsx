13f057072326c854a6f852274ffb457bcb80bd04
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-03-11 17:03:29 -0600

    hidden input

--- src/Components/utils/SelectInput.tsx
+++ src/Components/utils/SelectInput.tsx
@@ -2,11 +2,12 @@ interface SelectInputProps {
     options: {value: string | number, label: string}[]
     name: string;
     value: string;
-    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void,
+    hidden?: boolean
 }
 function SelectInput(props: SelectInputProps) {
     return (
-        <select value={props.value} onChange={props.onChange} className="border border-gray-400 p-2" name={props.name}>
+        <select value={props.value} onChange={props.onChange} hidden={props.hidden} className="border border-gray-400 p-2" name={props.name}>
             {props.options.map((option) => (
                 <option key={option.value} value={option.value}>
                     {option.label}
