d7dbb85b1c26b5179d500de3f8bb56171f1b3a5b
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-03-16 15:39:23 -0600

    add react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import App from './App'
-import './index.css'
 import router from './router'
+import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
