6f0d35fd43e3a179222923a10d869c6169de1dd5
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-03-27 11:59:44 -0600

    logout button / showing - hiding menus

--- src/Pages/index-page.tsx
+++ src/Pages/index-page.tsx
@@ -1,6 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../Utils/UseAuth";
 
 function IndexPage() {
+    const { auth, logout } = useAuth()
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        logout!()
+        navigate('/')
+    }
     return (
         <>
             <ul className="flex flex-col gap-5">
@@ -11,47 +18,56 @@ function IndexPage() {
                         </button>
                     </Link>
                 </li>
+                {auth.user.role == '1' ? null : <>
+                    <li>
+                        <Link to={'consumption-sheet-details'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Detalles de Hojas de Consumo
+                            </button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'rooms'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Cuartos
+                            </button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'patients'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Patients
+                            </button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'products'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Products
+                            </button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'staff'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Staff
+                            </button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'users'}>
+                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
+                                Users
+                            </button>
+                        </Link>
+                    </li>
+                </>
+                }
+
                 <li>
-                    <Link to={'consumption-sheet-details'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Detalles de Hojas de Consumo
-                        </button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'rooms'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Cuartos
-                        </button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'patients'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Patients
-                        </button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'products'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Products
-                        </button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'staff'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Staff
-                        </button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'users'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Users
-                        </button>
-                    </Link>
+                    <button className="bg-red-500 rounded p-3 drop-shadow-md text-slate-200 font-bold tracking-widest" onClick={handleLogout}>
+                        Logout
+                    </button>
                 </li>
             </ul>
         </>
