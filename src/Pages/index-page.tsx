eee8dd8f836afd71b1331f43bcf2de86ba737634
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-04-04 02:37:25 -0600

    pending state on navlinks

--- src/Pages/index-page.tsx
+++ src/Pages/index-page.tsx
@@ -1,5 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../Utils/UseAuth";
+
+interface NavState {
+    isActive: boolean
+    isPending: boolean
+}
 
 function IndexPage() {
     const { auth, logout } = useAuth()
@@ -8,67 +13,87 @@ function IndexPage() {
         logout!()
         navigate('/')
     }
+    const handleNavState = ({ isActive, isPending }: NavState) => {
+        return (isPending ? "bg-cyan-300" : isActive ? "bg-cyan-500" : "bg-cyan-400") + "bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest"
+    }
     return (
         <>
             <ul className="flex flex-col gap-5">
                 <li>
-                    <Link to={'consumption-sheets'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Hojas de Consumo
-                        </button>
-                    </Link>
+                    <NavLink to={'consumption-sheets'}>
+                        {({ isActive, isPending }) => (
+                            <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                Hojas de Consumo
+                            </button>
+                        )}
+
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to={'patients'}>
-                        <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                            Patients
-                        </button>
-                    </Link>
+                    <NavLink to={'patients'}>
+                        {({ isActive, isPending }) => (
+                            <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                Patients
+                            </button>
+                        )}
+                    </NavLink>
                 </li>
                 {auth.user.role == '1' ? null : <>
                     <li>
-                        <Link to={'consumption-sheet-details'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Detalles de Hojas de Consumo
-                            </button>
-                        </Link>
+                        <NavLink to={'consumption-sheet-details'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Detalles de Hojas de Consumo
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to={'rooms'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Cuartos
-                            </button>
-                        </Link>
+                        <NavLink to={'rooms'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Cuartos
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to={'products'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Products
-                            </button>
-                        </Link>
+                        <NavLink to={'products'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Products
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to={'staff'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Staff
-                            </button>
-                        </Link>
+                        <NavLink to={'staff'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Staff
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to={'users'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Users
-                            </button>
-                        </Link>
+                        <NavLink to={'users'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Users
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                 </>
                 }{auth.user.role != '1' ? null : (
                     <li>
-                        <Link to={'/auth/profile'}>
-                            <button className="bg-cyan-400 rounded p-3 drop-shadow-md text-slate-600 font-bold tracking-widest">
-                                Profile
-                            </button>
-                        </Link>
+                        <NavLink to={'/auth/profile'}>
+                            {({ isActive, isPending }) => (
+                                <button className={(isPending ? "bg-cyan-700 text-slate-200" : isActive ? "bg-cyan-500 text-slate-200" : "bg-cyan-400 text-slate-600") + " rounded p-3 drop-shadow-md font-bold tracking-widest"}>
+                                    Profile
+                                </button>
+                            )}
+                        </NavLink>
                     </li>
                 )}
 
