b470d58a7eafb8d0404cc243f9f72ada62e0ba27
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-04-24 11:59:30 -0600

    remove selected property

--- src/Components/utils/SelectInput.tsx
+++ src/Components/utils/SelectInput.tsx
@@ -10,7 +10,7 @@ function SelectInput(props: SelectInputProps) {
     return (
         <select value={props.value} onChange={props.onChange} hidden={props.hidden} className="border border-gray-400 p-2" name={props.name} disabled={props.disabled}>
             {props.options.map((option) => (
-                <option key={option.value} value={option.value} selected={option.value == props.value}>
+                <option key={option.value} value={option.value}>
                     {option.label}
                 </option>
             ))}
