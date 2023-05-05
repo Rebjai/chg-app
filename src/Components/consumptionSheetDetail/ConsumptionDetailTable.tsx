9b03ed120cdb35e49906de937a2ec3902f97f386
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-05-04 23:33:21 -0600

    enable update on consumption details

--- src/Components/consumptionSheetDetail/ConsumptionDetailTable.tsx
+++ src/Components/consumptionSheetDetail/ConsumptionDetailTable.tsx
@@ -3,6 +3,8 @@ import { useFetcher, useParams } from "react-router-dom";
 import ConsumptionSheetDetail from "../../Interfaces/consumptionSheetDetail.interface";
 import { useAuth } from "../../Utils/UseAuth";
 import deleteIcon from "../../assets/delete-icon.svg";
+import editIcon from "../../assets/edit-icon.svg";
+import { useState } from "react";
 
 interface ConsumptionDetailTableProps {
     consumptionDetails: ConsumptionSheetDetail[]
@@ -12,8 +14,14 @@ function ConsumptionDetailTable({ consumptionDetails }: ConsumptionDetailTablePr
     const { t } = useTranslation()
     const { auth } = useAuth()
     const fetcher = useFetcher()
-    return (
+    const [enabledInputsIndex, setEnabledInputsIndex] = useState(-1);
 
+    const enableInputs = (index: number) => {
+        setEnabledInputsIndex(index);
+    };
+
+
+    return (
         <div className="bg-green-100 rounded p-5">
             <table className="min-w-full border-spacing-5">
                 <thead>
@@ -21,33 +29,63 @@ function ConsumptionDetailTable({ consumptionDetails }: ConsumptionDetailTablePr
                         <th className="px-3">Date</th>
                         <th className="px-3">Product</th>
                         <th className="px-3">quantity</th>
-                        {auth.user.role == '1' ? null : <th className="px-3">price</th>}
-                        {auth.user.role == '1' ? null : <th className="px-3">Actions</th>}
+                        {auth.user.role === "1" ? null : <th className="px-3">price</th>}
+                        {auth.user.role === "1" ? null : <th className="px-3">Actions</th>}
                     </tr>
                 </thead>
                 <tbody>
-
-                    {consumptionDetails?.length ? consumptionDetails.map(detail => (
+                    {consumptionDetails?.length ? consumptionDetails.map((detail, index) => (
                         <tr key={detail.id}>
-                            <td className="px-2">{detail.created_at?.toLocaleString()}</td>
+                            <td className="px-2">{enabledInputsIndex === index ? (
+                                <input
+                                    type="text"
+                                    defaultValue={detail.created_at?.toLocaleString()}
+                                    className="border rounded p-1"
+                                />
+                            ) : (
+                                detail.created_at?.toLocaleString()
+                            )}</td>
                             <td className="px-2">{detail.product?.name}</td>
-                            <td className="px-2">{detail.quantity}</td>
+                            <td className="px-2">{enabledInputsIndex === index ? (
+                                <input
+                                    type="number"
+                                    defaultValue={detail.quantity}
+                                    className="border rounded p-1"
+                                />
+                            ) : (
+                                detail.quantity
+                            )}</td>
                             {auth.user.role == '1' ? null : <td className="px-2">{detail.total}</td>}
                             {auth.user.role == '1' ? null :
-                                <td className="px-2">
-                                    <fetcher.Form method="delete" action={"/consumption-sheet-details/" + detail.id} className="">
-                                        <button type="submit" className="inline-block max-w-[100px] w-6/12">
-                                            <img src={deleteIcon} alt={t('delete')!} />
-                                        </button>
-                                    </fetcher.Form>
+                                <td className="">
+                                    <div className="flex">
+
+                                        <fetcher.Form method="delete" action={"/consumption-sheet-details/" + detail.id} className="flex m-auto justify-center">
+                                            <button type="submit" className="inline-block max-w-[100px] w-6/12">
+                                                <img src={deleteIcon} alt={t('delete')!} />
+                                            </button>
+                                        </fetcher.Form>
+                                        {/* <div className="flex">
+                                            <button type="button" onClick={() => enableInputs(index)} className="inline-block max-w-[100px] w-6/12">
+                                                {enabledInputsIndex === index ? (
+                                                    <img src={deleteIcon} alt={t('save')!} />
+                                                ) : (
+                                                    <img src={editIcon} alt={t('update')!} />
+                                                )}
+                                            </button>
+                                        </div> */}
+                                    </div>
                                 </td>}
                         </tr>)
                     ) : (<tr>
                         <td colSpan={4} className="text-center">No data found</td>
                     </tr>)}
+
                 </tbody>
             </table>
-        </div>);
+        </div>
+    );
+
 }
 
 export default ConsumptionDetailTable;
