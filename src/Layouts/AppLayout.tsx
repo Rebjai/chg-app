f139a9bee07b440ea768f88177b6ec0372f2967a
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-07-03 23:35:57 -0600

    header title ui fixes

--- src/Layouts/AppLayout.tsx
+++ src/Layouts/AppLayout.tsx
@@ -5,14 +5,14 @@ import chgLogo from '../assets/chg-app.svg'
 function AppLayout() {
     const pageTitle = location.pathname.slice(1);
     const navigation = useNavigation()
-    
+
     return (
         <div className="App">
-            <header className="flex justify-center items-center gap-5">
+            <header className="flex flex-col justify-center items-center gap-5">
 
                 <Link to={'/'} className="max-w-[100px] w-2/12" ><img src={chgLogo} className="w-full" alt="CHG APP logo" /></Link>
 
-                <h2 className="text-2xl font-bold text-purple-700 ml-4">{pageTitle}</h2>
+                <h2 className="text-2xl font-bold text-purple-700 ml-4 mb-4">{!!pageTitle ? pageTitle : 'CHG-APP'}</h2>
             </header>
             <Suspense fallback={<h1 className="text-center font-bold my-5">loading...</h1>}>
                 <div className="flex flex-col text-center items-center">
