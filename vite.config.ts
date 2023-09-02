9202dd4342ff91b4eb5e5557ea5fe41aff2113a0
rebjai <15723342+Rebjai@users.noreply.github.com>
2023-09-02 05:53:12 -0600

    config proxy to API backend

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: { outDir: '../chg-consumption-sheet/client' },
+  server:{
+    proxy: {'/api': {
+      target: 'http://localhost:3000',
+      // changeOrigin: true,
+    }}
+  }
 })
